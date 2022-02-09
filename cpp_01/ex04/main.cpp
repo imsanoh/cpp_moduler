ea1aee7e6820b9ef4b7cd27f0c282dc72e2f00f0
Sangwoo Oh <sanoh@c2r6s3.42seoul.kr>
2022-02-09 16:14:14 +0900

    sanoh

--- cpp_01/ex04/main.cpp
+++ cpp_01/ex04/main.cpp
@@ -1,15 +1,47 @@
 #include <string>
+#include <iostream>
+#include <fstream>
 
-int main ()
+replaces
+
+int main()
 {
-	std::string	s = "abc"; //s가 string 클래스 이걸 쓴다. 
-	s.replace()    //x 맴버 함수 
-	s.length()
-	. ... //string 안에 있는 내자ㅏㅇ 함수를 쓴다.
+	//문자열 = s1;
+	//문자열2 = s2;
 
-	char   *str
+	std::ifstream ifs("file");
+	std::string	s1;
+	std::string	s2;
+	ifs >> s1 >> s2;
 
-	strlen(str);
+	std::cout << s1 << " " << s2 << std::endl;
+	ifs.close();
+
+	std::ofstream ofs("file.replace");
+	ofs << "somethig" << std::endl;
+	ofs.close();
 }
 
+//대체 해주는 함수를 하나 만들자!
+
+// int main()
+// {
+// 	std::ifstream ifs("somethings")
+
+// 	std::string s1;
+// 	std::string s2;
+
+// 	std::ofstream ofs("somethings.replace")
+// 	ofs.close();  
+
+// }
+
+//./program
+//s1 -> s2;
+//프로그램돌아가면 
 // 내장함수를 이용해서 replace 같은 친구를 사용하는거임
+
+//ifstream?
+
+//파일은 존재 해야 하고
+//그 안에 있는 인자를 바꾸는 프로그램 만드는거 임
--- cpp_example/ex01-3/filestreams.cpp
+++ cpp_example/ex01-3/filestreams.cpp
@@ -3,18 +3,26 @@
 
 int main()
 {
-	std::ifstream	ifs("numbers");
+	std::ifstream	ifs("numbers");//읽을거니까 뒤에 문자열을 줌
+	//예외처리
 	unsigned int	dst;
 	unsigned int	dst2;
-	ifs >> dst >> dst2;
+	ifs >> dst >> dst2; //띄어쓰기 or 개행 
 
 	std::cout << dst << " " << dst2 << std::endl;
 	ifs.close();
 
-	std::ofstream ofs("test.out");
+	std::ofstream ofs("numbers.replace");
 	ofs << "i like penies a whole damn lot" << std::endl;
 	ofs.close();
 }
 
+//s1 abc working abc ->cdf
+//cdf ->ofs 저장소 
+//대체 되는 파일을 replace
+//replace => 만들어야힘
+
 
 //ifstram 이랑 ofstream 이랑 뭔지 알아야함
+//ifstream : 파일 읽기 함수 -> 결국 키보드로 가져오는것이 아닌 파일로 가져옴
+//ofstream : 파일에 쓰기 함수 -> 키보드가 아닌 파일로 쓴다.
