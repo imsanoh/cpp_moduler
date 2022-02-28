a014b086f8486d483c6c86ef1eb33c79fb7855ce
Sangwoo Oh <sanoh@c4r4s5.42seoul.kr>
2022-02-28 19:27:40 +0900

    imsanoh

--- cpp_03/ClapTrap.hpp
+++ cpp_03/ClapTrap.hpp
@@ -7,19 +7,21 @@
 class ClapTrap
 {
 	private:
-		void name;
-		int hitpoint;
-		void Energy;
-		void Attack;
+		std::string name;
+		unsigned int hitpoint;
+		unsigned int Energy;
+		unsigned int Attack;
 	public:
 		ClapTrap();
+		ClapTrap( std::string name ); //Name (Parameter of constructor) 이 이야기 입니다.
+		ClapTrap(const ClapTrap& ClapTrap)
 		~ClapTrap();
 		
-		ClapTrap& operator=(const ClapTrap &ref);
+		ClapTrap& operator=(const ClapTrap& claptrap);
 		
 		void attack(std::string const & target)
 		void takeDamage(unsigned int amount)
 		void beRepaired(unsigned int amount)
-};
+}; //안나와 있어서 안씀
 
 #endif
