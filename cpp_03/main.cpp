a8ce66817ce46831b29c2ee7bbc2e9ae172412f7
오상우 <sanoh@osang-uui-MacBookAir.local>
2022-03-01 17:43:59 +0900

    imsanoh

--- cpp_03/main.cpp
+++ cpp_03/main.cpp
@@ -7,5 +7,11 @@ int	main(void)
 	ClapTrap	c("C");
 
 	a.attack("B"); // 'a' 가 'B'를 공격 했다.
-	b.takeDamage(a.getDamage()); //
+	b.takeDamage(a.getDamage()); //td->
+	b.attack("A");
+	a.takeDamge(a.getDamage());
+	c.attack("B");
+	b.takeDamage(a.getDamage());
+	a.beRepaired(25);//맨처음으로 돌리는거
+	return (0);
 }
