8ee5c8ca4411da47652a0ef0c67f3bd641cd5247
오상우 <sanoh@osang-uui-MacBookAir.local>
2022-03-11 21:22:55 +0900

    imsanoh

--- cpp_03/ClapTrap.cpp
+++ cpp_03/ClapTrap.cpp
@@ -6,30 +6,40 @@ ClapTrap::ClapTrap()
 	hitpoint = 10;
 	energy = 10;
 	damage = 0;
-}
+	std::cout << "[ClapTrap] : ";
+	std::cout << name << " : ClapTrap default constructor is called" << std::endl;
+};
 
 ClapTrap::ClapTrap( std::string str )
 {
-	*this = ref;
+	name = str;
+	hitpoint = 10;
+	energy = 10;
+	damage = 0;
+	std::cout << "[ClapTrap] : ";
+	std::cout << name << " : ClapTrap string constructor is called" << std::endl;
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap &claptrap)
+{
+	this->name = claptrap.name;
+	this->hitpoint = claptrap.hitpoint;
+	this->energy = claptrap.energy;
+	this->damage = claptrap.damage;
+	std::cout << "[ClapTrap] : ";
+	std::cout << name << " : ClapTrap destructor is called" << std::endl;
+	return (*this);
 }
 
 ClapTrap::~ClapTrap()
 {
-}
-
-ClapTrap& ClapTrap::operator=(const ClapTrap &ref)
-{
-	return (*this);
-}
-
-void	ClapTrap::attack(std::string const& target)
-{
-	std::cout << "ClapTrap : " << std::endl;
-	std::cout << name << "attack " 
+	std::cout << "[ClapTrap] : ";
+	std::cout << name << " : ClapTrap destructor is called" << std::endl;
 }
 
 void ClapTrap::takeDamage(unsigned int amount)
 {
+	std::cout << "[ClapTrap] : ";
 	if (amount < hitpoint)
 	{
 		hitpoint -= amount;
@@ -42,11 +52,16 @@ void ClapTrap::takeDamage(unsigned int amount)
 	}
 }
 
-void ClapTrap::beRepaired(unsigned int amount)
+void	ClapTrap::beRepaired(unsigned int amount)
 {
-	
+	std::cout << "[ClapTrap] : ";
+	hitpoint += amount;
+	std::cout << name " is repaired " << amount << " points of heal! [HP] :" << hitpoint << std::endl;
 }
 
-
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	return (damage);
+}
 //생성자의 규칙 알아보기
-//맴버 초기화 리스트 
+//맴버 초기화 리스트
--- cpp_03/ClapTrap.hpp
+++ cpp_03/ClapTrap.hpp
@@ -9,19 +9,21 @@ class ClapTrap
 	private:
 		std::string name;
 		unsigned int hitpoint;
-		unsigned int Energy;
-		unsigned int Attack;
+		unsigned int energy;
+		unsigned int attack;
+		unsigned int damage;
 	public:
 		ClapTrap();
 		ClapTrap( std::string name ); //Name (Parameter of constructor) 이 이야기 입니다.
-		ClapTrap(const ClapTrap& ClapTrap)
-		~ClapTrap();
-		
+		ClapTrap(const ClapTrap& ClapTrap);
+		~ClapTrap();//소멸자 입니다.
+
 		ClapTrap& operator=(const ClapTrap& claptrap);
-		
-		void attack(std::string const & target)
-		void takeDamage(unsigned int amount)
-		void beRepaired(unsigned int amount)
+
+		void attack(std::string const & target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+		unsigned int getDamage(void) const;
 }; //안나와 있어서 안씀
 
 #endif
